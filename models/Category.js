22c20ee540cea17a6664e5d753022493f5ae9ab8
Matthew Lawrence <Mattplawrence@gmail.com>
2022-01-31 17:55:34 -0500

    working on models

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,23 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
