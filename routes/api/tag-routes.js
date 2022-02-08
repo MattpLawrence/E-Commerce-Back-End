2caf42147af5a95c34a3ee712325d42ae03900dd
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-08 17:19:38 -0500

    update readme

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -25,25 +25,39 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
   try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
   try {
+    Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   try {
+    const deleteProduct = await tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deleteProduct);
   } catch (err) {
     res.status(500).json(err);
   }
